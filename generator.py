f3c98b4bcfefa2db722c33bad7d9b529cc248841
jav.js <48942328+duduskying@users.noreply.github.com>
2026-09-21 11:24:17 +0800

    Update generator.py

--- generator.py
+++ generator.py
@@ -30,7 +30,7 @@ import yaml
 
 VERSION = "v7"
 OUTPUT_PATH = Path("output/clash.yaml")
-TEST_URL = "https://cp.cloudflare.com/generate_204"
+TEST_URL = "https://www.google.com/generate_204"
 SOURCE_TIMEOUT = 25
 LATENCY_TIMEOUT_MS = 5000
 MAX_RETRIES = 3
